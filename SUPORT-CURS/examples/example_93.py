5751ed464d22fc4cee1e95053daa9a409dd186c3
Danna94 <danna.oancea@yahoo.com>
2026-03-12 17:27:45 +0200

    curs 2

--- SUPORT-CURS/examples/example_93.py
+++ SUPORT-CURS/examples/example_93.py
@@ -13,23 +13,3 @@ for k in range(n * m):
     t += str(k) + " A[" + str(i) + "][" + str(j
     t += str(A[i][j]) + "\n"
     print(t)
-    the
-    number
-    of
-    columns(m).The
-    code
-    then
-    u
-    concludes
-    by
-    printing
-    the
-    contents
-    of
-    string
-    and column
-    indices
-    for traversing a two-dime
-    value len(A), which represents the number o
-    the value len(A[0]), which represents the nu
-    A using A[i][j].The values are concatenated
