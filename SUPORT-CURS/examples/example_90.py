9143a2e9b6f8f4ee5572812e1a8c7a6725063ecf
Deea95Gaming <deea95.gaming@gmail.com>
2026-03-12 17:06:31 +0200

    - Andreea - 12.03.2026 - aranjare exemplul 89 si 90

--- SUPORT-CURS/examples/example_90.py
+++ SUPORT-CURS/examples/example_90.py
@@ -1,19 +1,15 @@
-          A = [
-            [
-               ["a", 88, 146],
-               ["b", 34, 124],
-               ["c", 96, 564],
-               [100, 12, "d"],
-            ],
-            [
-               ["e", 48, 996],
-               ["f", 34, 554],
-               ["g", 26, 884],
-               [111, 92, "h"],
-            ]
-          ]
-          print(A[1][2])
-        ilarly, the second nested array A[1] also con
-        The last line of code, print(A[1][2]), is tryin
-        Specifically, it accesses the third sub-array ([
-        print its contents. In this case, it would print
+A = [
+    [
+        ["a", 88, 146],
+        ["b", 34, 124],
+        ["c", 96, 564],
+        [100, 12, "d"],
+    ],
+    [
+        ["e", 48, 996],
+        ["f", 34, 554],
+        ["g", 26, 884],
+        [111, 92, "h"],
+    ]
+]
+print(A[1][2])
