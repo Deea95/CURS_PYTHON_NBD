0d444ce54bae558eeae0dc1929bae36218cb92ab
Danna94 <danna.oancea@yahoo.com>
2026-04-02 13:28:59 +0300

    curs 3

---
+++ Danna/curs-04-Dana.py
@@ -0,0 +1,16 @@
+# # Replacement for repeat loops with recursion
+# # replacement for                  Output:
+# # repeat loops.
+# def for_loop(a, b, r):
+#     a += 1
+#     # do stuff from
+#     r += 5
+#     # to here
+#     if a >= b:
+#         return r
+#     else:
+#         return for_loop(a, b, r)
+#
+# a = for_loop(0, 7, 0)
+# print(a)
+
