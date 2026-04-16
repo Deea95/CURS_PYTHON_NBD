5d10567319c56d39fb16fc000f31793dbe5713f5
CristinaO94 <krys_tina_2006@yahoo.com>
2026-04-16 17:11:30 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	SUPORT-CURS/examples/example_183.py
    #	SUPORT-CURS/examples/example_188.py

--- Danna/curs-05-Dana.py
+++ Danna/curs-05-Dana.py
@@ -375,32 +375,98 @@ from itertools import count
 # print(alpha('uiuhd87wqsaidhsad'))
 
 
-
+# # exemplu cu matrice pt exercitiul de mai sus
+# c = [
+#     [1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+#     [1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
+#     [1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+#     [0, 1, 0, 0, 1, 1, 1, 0, 0, 1],
+#     [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
+#     [1, 0, 1, 1, 1, 1, 0, 1, 0, 0],
+#     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+#     [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+#     [1, 1, 0, 0, 0, 0, 1, 0, 0, 1]
+# ]
 #
-c = [
-    [1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
-    [1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
-    [0, 1, 0, 0, 1, 1, 1, 0, 0, 1],
-    [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
-    [1, 0, 1, 1, 1, 1, 0, 1, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
-    [1, 1, 0, 0, 0, 0, 1, 0, 0, 1]
-]
+#
+# def matrix_alphabet(t):
+#     a = []
+#     n = len(t)
+#     m = len(t[0])
+#     for i in range(n):
+#         for j in range(m):
+#             q = 1
+#             for k in range(len(a) + 1):
+#                 if k < len(a) and t[i][j] == a[k]:
+#                     q = 0
+#             if q == 1:
+#                 a.append(t[i][j])
+#     return a
+# print(matrix_alphabet(c))
 
 
-def matrix_alphabet(t):
-    a = []
-    n = len(t)
-    m = len(t[0])
+
+
+# # Inside the loop, it assigns the value of b[i] t the number of elements), and stores it in the
+# # that value to b[r] and increments the value ranges and is written for this book.To my
+#
+# a = {}
+# b = [3, 6, 2, 78, 99, 1, 4]
+#
+# r = 0
+# n = len(b)
+#
+# for i in range(n):
+#     a[b[i]] = b[i]
+#
+# m = max(a.keys()) + 1
+#
+# for j in range(m):
+#     if j in a:
+#         b[r] = a[j]
+#         r += 1
+# print(b)
+
+
+
+# # Return an array with proportions (relative frequencies)
+#
+# def p(a):
+#     max_value = max(a)
+#     n = len(a)
+#     m = 100
+#
+#     # Preaallocate the list
+#     # with the required size.
+#
+#     t = [''] * n
+#     for i in range(n):
+#         t[i] = str(round((m / max_value) * a[i])) + '%'
+#
+#     return t
+#
+# ......nu am apucat sa scriu tot
+
+
+# Pearson correlation coefficient
+
+def p(a, b):
+    n = len(a)
+    m = [0, 0]
     for i in range(n):
-        for j in range(m):
-            q = 1
-            for k in range(len(a) + 1):
-                if k < len(a) and t[i][j] == a[k]:
-                    q = 0
-            if q == 1:
-                a.append(t[i][j])
-    return a
-print(matrix_alphabet(c))
+        m[0] += a[i]
+        m[1] += b[i]
+    m[0] = m[0] / n  # mean a.
+    m[1] = m[1] / n  # mean b.
+    s0 = 0
+    s1 = 0
+    s2 = 0
+    for i in range(n):
+        s0 += (a[i] - m[0]) * (b[i]-m[1])
+        s1 += (a[i] -m[0]) ** 2
+        s2 += (b[i] - m[1]) ** 2
+    r = s0 / (s1 * s2) ** 0.5
+    return r
+a = [6, 8, 10]
+b = [12, 10, 20]
+print(p(a, b))
